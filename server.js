d4b8ecb70e402b086179352462b3e45093b21054
mcdobr <mircea.dobreanu@gmail.com>
2018-12-02 12:12:40 +0200

    small refactoring

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-let oracledb = require('oracledb')
-let express = require('express')
-let config = require('./config.js');
-
-
-const PORT_NO = 10000;
+const oracledb = require('oracledb')
+const express = require('express')
+const config = require('./config.js');
 const app = express();
 
 // const bills = require('./bill');
@@ -21,10 +18,8 @@ oracledb.createPool(
             console.log('Connected to Oracle Database');
             console.log('Maximum pool listeners: ' + pool.getMaxListeners());
 
-
-
             //app.use('/api/bills', bills);
-            app.use('/api/categories', categories(pool.getConnection()));
+            app.use('/api/', categories(pool.getConnection()));
             //app.use('/api/historicquantities', historicQuantites);
             //app.use('/api/products', product);
         }
@@ -33,40 +28,3 @@ oracledb.createPool(
 
 
 app.listen(config.port);
-
-
-/*
-app.get('/', function (request, response) {
-    response.send('Hi there');
-});
-
-oracledb.getConnection({
-        user: 'shop_admin',
-        password: 'shop',
-        connectString: 'localhost:1521/ORCLPDB1'
-    },
-    function (error, connection) {
-        if (error) {
-            console.error(error.message);
-        } else {
-            console.log('Connected to Oracle Database');
-            connection.execute(
-                `SELECT *
-                FROM category
-                WHERE id = :id`, [2],
-                (error, result) => {
-                    if (error) {
-                        console.error(error.message);
-                        releaseConnection(connection);
-                        return;
-                    } else {
-                        console.log(result.rows);
-                        console.log(typeof result);
-                        console.log(typeof result.rows);
-                        releaseConnection(connection);
-                    }
-                }
-            )
-        }
-    }
-)*/
