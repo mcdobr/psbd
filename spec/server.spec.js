3c02e3791e5e8d3f7e076c0e2271fe6c26efb5e5
mcdobr <mircea.dobreanu@gmail.com>
2018-12-29 11:56:41 +0200

    added a simple unit test

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -5,6 +5,7 @@ describe('Server', () => {
     var server;
     beforeAll(() => {
         server = require('../server');
+        // console.log(server.address());
     });
     afterAll(() => {
         server.close();
@@ -14,16 +15,15 @@ describe('Server', () => {
     describe('GET /', () => {
         let data = {};
         beforeAll((done) => {
-            request.get('http://localhost:10000'), (error, response, body) => {
+            request.get('http://localhost:10000', (error, response, body) => {
                 data.status = response.statusCode;
                 data.body = body;
                 done();
-            }
+            });
         });
 
         it('Status 404 - Not Found', () => {
             expect(data.status).toBe(404);
         });
-
     });
 });
